c3387b38e9b35023a40c78ad53bad5d484d2b53c
Honwiy <444217547@qq.com>
2018-03-22 01:21:24 +0800

    接口补充到接口6

--- zhihudaily-api.py
+++ zhihudaily-api.py
@@ -14,7 +14,7 @@ def latest():
     result.headers['Access-Control-Allow-Origin']='*'
     return result
 
-@app.route('/content/<id>',)
+@app.route('/content/<id>')
 def content(id):
     headers = {
     'User-Agent': 'Mozilla/5.0'
@@ -24,6 +24,29 @@ def content(id):
     result = make_response(r.text)
     result.headers['Access-Control-Allow-Origin']='*'
     return result
+
+@app.route('/before/<date>')
+def before(date):
+    headers = {
+    'User-Agent': 'Mozilla/5.0'
+    }
+    r = requests.get("https://news-at.zhihu.com/api/4/news/before/20131119{date}".format(date=date),headers = headers)
+    
+    result = make_response(r.text)
+    result.headers['Access-Control-Allow-Origin']='*'
+    return result
+
+@app.route('/story-extra/<id>')
+def storyExtra(id):
+    headers = {
+    'User-Agent': 'Mozilla/5.0'
+    }
+    r = requests.get("https://news-at.zhihu.com/api/4/story-extra/#{id}".format(id=id),headers = headers)
+    result = make_response(r.text)
+    result.headers['Access-Control-Allow-Origin']='*'
+    return result
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000,debug=True)
